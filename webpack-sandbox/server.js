2f3cb8193be78e6d284ba4b033a4b2b5ef7c1310
Brian Fajardo <bmf5@hotmail.com>
2017-06-07 19:07:16 -0400

    Setup Webpack middleware and Node server in production

--- webpack-sandbox/server.js
+++ webpack-sandbox/server.js
@@ -1,16 +1,37 @@
 const express = require('express')
-const webpack = require('webpack')
-const webpackConfig = require('./webpack.config.js')
-const webpackMiddleware = require('webpack-dev-middleware')
+const path = require('path')
 
 const app = express()
 
-// webpackConfig instructs webpack how to behave.
-// webpack module is passed to the middleware.
-// webpackMiddleware is used in Express application
+// Server routes, express middleware, etc, should be defined
+// before webpack configuration. Specifically before `res.sendFile`.
 
-app.use(webpackMiddleware(webpack(webpackConfig)))
+if (process.env.NODE_ENV !== 'production') {
 
+  // Setting block-scope imports
+  const webpack = require('webpack')
+  const webpackConfig = require('./webpack.config.js')
+  const webpackMiddleware = require('webpack-dev-middleware')
 
+  // webpackConfig instructs webpack how to behave.
+  // webpack module is passed to the middleware.
+  // webpackMiddleware is used in Express application.
+  app.use(webpackMiddleware(webpack(webpackConfig)))
 
-app.listen(8000, () => console.log('Listening on localhost:8000'))
+} else {
+
+  // Allows express to serve dist directory freely to any request.
+  app.use(express.static('dist'))
+
+  // On GET request to any route (*) of server, send index.html file.
+  // Works with BrowserRouter (React-Router)~ YES!
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
+}
+
+// Production providers will set their own PORT
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Listening on ${process.env.PORT || 'localhost:8000'}`)
+})
